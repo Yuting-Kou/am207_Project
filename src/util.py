6071b934d119c85f12e02eaef048ae0d170a783a
kou yuting <yutingkou@outlook.com>
2019-11-24 16:46:19 -0500

    todo: update initializer using model object function.

--- src/util.py
+++ src/util.py
@@ -1,12 +1,12 @@
+from model import Model
 
-def initializer(model,X,y):
+
+def initializer(model, X, y):
     """
     get a pretrained SGD result w0
     :param model: model
+    :type model: Model
     :param X: Data X
     :param y: Data y
     """
     pass
-
-
-
