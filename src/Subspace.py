17b28031f5d2b91f618fa7900d4676a3ddcbfa35
Susana Xu <yimingxu@g.harvard.edu>
2019-11-24 22:08:54 -0500

    update pca and swa

--- src/Subspace.py
+++ src/Subspace.py
@@ -1,7 +1,6 @@
 from abc import ABC
 
 from autograd import numpy as np
-# from sklearn.decomposition import TruncatedSVD
 from sklearn.utils.extmath import randomized_svd
 
 from SWA import SWA
@@ -54,12 +53,12 @@ class RandomSpace(Subspace):
         self.subspace = np.random.randn(self.n_parameters, n_subspace)
         self.model = model
         self.w_hat = np.zeros((self.n_parameters, 1))
-
-    def collect_vector(self, X, y, method="T"):
-        """set shift vector as SWA results"""
-        myswag = SWA(model=self.model)
-        myswag.train_SGD(X=X, y=y, method=method)
-        self.w_hat = myswag.get_w_swa()
+        
+    def collect_vector(self, X, y, method="T", lr = 0.02, T = 2000, c = 10, max_rank = 20):
+        """set shift vector as SWA results
+        params info see SWA.py"""
+        self.myswag = SWA(self.model, X, y, method, lr, T, c, max_rank)
+        self.w_hat = self.myswag.get_w_swa()
 
     def get_space(self):
         """
@@ -72,47 +71,41 @@ class RandomSpace(Subspace):
 
 @Subspace.register_subclass('pca')
 class PCASpace(Subspace):
-    def __init__(self, model:Model, PCA_rank=20, max_rank=20):
+    def __init__(self, model:Model, n_subspace=20):
         """
         Initialize random subspace method
         :param model: # of dimension of original weight space
         :type model: Model
-        :param PCA_rank # of dimension of low_dim representation (K in paper's algorithm2)
-        :param max_rank # of maximum columns in deviation matrix (M in paper's algorithm2)
+        :param n_subspace: # of dimension of low_dim representation (small)
         """
         super(PCASpace, self).__init__()
 
-        self.n_parameters = len(model.get_weights())
-        self.PCA_rank = PCA_rank
-        self.max_rank = max_rank
+        self.n_parameters = model.get_D()
+        self.n_subspace = n_subspace
+        self.model = model
+        self.w_hat = np.zeros((self.n_parameters, 1))
 
-        self.rank = 1
-        # final shape should be (max_rank, n_parameters)
-        self.deviation_matrix = np.ones(self.n_parameters).reshape(1, -1)
 
-    def collect_vector(self, vector):
-        if self.rank + 1 > self.max_rank:
-            self.deviation_matrix = self.deviation_matrix[1:, :]  # keep the last (max_rank - 1) deviations
-        self.deviation_matrix = np.concatenate([self.deviation_matrix, vector.reshape(1, -1)], axis=0)
-        self.rank = min(self.rank + 1, self.max_rank)  # update the matrix rank
+    def collect_vector(self, X, y, method="T", lr = 0.02, T = 2000, c = 10, max_rank = 20):
+        """set shift vector as SWA results
+        params info see SWA.py"""
+        self.myswag = SWA(self.model, X, y, method, lr, T, c, max_rank)
+        self.w_hat = self.myswag.get_w_swa()
+ 
 
     def get_space(self):
-        deviation_matrix = self.deviation_matrix.copy()
+        self.A = self.myswag.get_A()
+        _, s, Vt = randomized_svd(self.A, n_components=self.n_subspace)
+        self.subspace = (np.diag(s)@Vt).T
 
-        # deviation_matrix /= (max(1, self.rank)-1)**0.5
-        # pca_rank = max(1, min(self.PCA_rank, self.rank))
-        # pca_decomp = TruncatedSVD(n_components=self.PCA_rank)
-        # pca_decomp.fit(deviation_matrix) # PCA based on randomized SVD
-
-        _, s, Vt = randomized_svd(deviation_matrix, n_components=self.PCA_rank)
-
-        return s[:, None] * Vt
+        return self.subspace, self.w_hat
 
 
 if __name__ == '__main__':
     # test
     print("All possible models:{}".format(Model.modeltype))
     data = np.load(r'..\example\data.npy')
+    # data = np.load('data.npy')
     x, y = data[:, 0], data[:, 1]
 
     alpha = 1
@@ -134,7 +127,7 @@ if __name__ == '__main__':
                     'activation_fn': h}
 
     my_nn = Model.create(submodel_type="Feedforward", architecture=architecture)
-    my_subspace = Subspace.create(subspace_type="random", model=my_nn, n_subspace=2)
+    my_subspace = Subspace.create(subspace_type="pca", model=my_nn, n_subspace=2)
 
     my_subspace.collect_vector(X=x, y=y)
     P, w = my_subspace.get_space()
@@ -142,6 +135,6 @@ if __name__ == '__main__':
 
 
     z = np.random.randn(2,1)
-    print(my_nn.get_likelihood(X=x.reshape(1,-1), z=z, P=P, w_hat=w))
+    print(my_nn.get_likelihood(X=x.reshape(1,-1), z=z, P=P, w_hat=w).shape)
 
 
